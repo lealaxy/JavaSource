e2443e7c32cce22eebcf8f3171b45c1eb30d84f3
peter <295741554@qq.com>
2020-04-04 14:13:18 +0800

    添加java8源码

---
+++ src/test/hashmap/Test.java
@@ -0,0 +1,18 @@
+package test.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description:
+ * @author: peterli
+ * @date: 2020/4/3 11:30
+ */
+public class Test {
+    public static void main(String[] args) {
+        Map<String, String> test = new HashMap();
+        test.put("dd", "dd");
+        test.put("aa", "aa");
+        test.put("bb", "bb");
+    }
+}
